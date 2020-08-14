a4b89aa3e418a1d239f257796b1ec33318c7b16c
Habib Syuhada <habibsumedang@gmail.com>
2020-08-14 15:02:04 +0700

    All content on dashboard linked to firebase

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,26 +2,26 @@
   <div class="row my-4">
     <div class="col-xl-3 col-md-3">
     	<div class="bg-white text-center p-3 shadow">
-    		<h4 class="header-title mb-0"><b>Today's Order</b></h4>
-    		<h1 class="my-2 font-weight-bold"><?php echo rand(1,9) ?></h1>
+    		<h4 class="header-title mb-0"><b>Today's Request</b></h4>
+    		<h1 class="my-2 font-weight-bold total-request-today">0</h1>
     	</div>
     </div>
     <div class="col-xl-3 col-md-3">
     	<div class="bg-white text-center p-3 shadow">
-    		<h4 class="header-title mb-0"><b>This Month's Order</b></h4>
-    		<h1 class="my-2 font-weight-bold"><?php echo rand(10,30) ?></h1>
+    		<h4 class="header-title mb-0"><b>This Month's Request</b></h4>
+    		<h1 class="my-2 font-weight-bold total-request-month">0</h1>
     	</div>
     </div>
     <div class="col-xl-3 col-md-3">
     	<div class="bg-white text-center p-3 shadow">
-    		<h4 class="header-title mb-0"><b>This Year's Order</b></h4>
-    		<h1 class="my-2 font-weight-bold"><?php echo rand(60,110) ?></h1>
+    		<h4 class="header-title mb-0"><b>This Year's Request</b></h4>
+    		<h1 class="my-2 font-weight-bold total-request-year">0</h1>
     	</div>
     </div>
     <div class="col-xl-3 col-md-3">
     	<div class="bg-white text-center p-3 shadow">
-    		<h4 class="header-title mb-0"><b>Summary All Order</b></h4>
-    		<h1 class="my-2 font-weight-bold"><?php echo rand(111,200) ?></h1>
+    		<h4 class="header-title mb-0"><b>Summary All Request</b></h4>
+    		<h1 class="my-2 font-weight-bold total-request-all">0</h1>
     	</div>
     </div>
   </div>
@@ -35,7 +35,7 @@
   <div class="row my-4">
     <div class="col-12">
     	<div class="bg-white p-3 shadow">
-    		<h3 class="font-weight-bold mb-2 text-center">In Progress Order</h3>
+    		<h3 class="font-weight-bold mb-2 text-center">In Progress Request</h3>
     		<table class="table table-bordered text-center datatables">
           <thead class="text-uppercase bg-primary">
             <tr class="text-white">
@@ -225,22 +225,59 @@
       json_service = data;
     }
     if(nama_var == 'biaya' || nama_var == 'service'){
-      var actual = 0;
+      var total_actual = 0;
+      var total_bensin = 0;
+      var total_angin = 0;
+      var total_parkir = 0;
+      var total_lain = 0;
+      var total_service = 0;
+      var total_actual_all = [];
       var d = new Date();
       var json_biaya_key = Object.keys(json_biaya);
       json_biaya_key.forEach(function(key) {
-        actual = actual + parseInt(json_biaya[key].Angin) + parseInt(json_biaya[key].Bensin) + parseInt(json_biaya[key].Lain) + parseInt(json_biaya[key].Parkir);
+        total_actual = total_actual + parseInt(json_biaya[key].Angin) + parseInt(json_biaya[key].Bensin) + parseInt(json_biaya[key].Lain) + parseInt(json_biaya[key].Parkir);
+        total_bensin = total_bensin + parseInt(json_biaya[key].Bensin);
+        total_angin = total_angin + parseInt(json_biaya[key].Angin);
+        total_parkir = total_parkir + parseInt(json_biaya[key].Parkir);
+        total_lain = total_lain + parseInt(json_biaya[key].Lain);
       });
-      console.log(actual);
       var json_service_key = Object.keys(json_service);
       json_service_key.forEach(function(key) {
         var date_service = json_service[key].Service_Month;
         var month_service = date_service.split("-");
         if((parseInt(month_service[1]) - 1) == d.getMonth()){
-          actual = actual + parseInt(json_service[key].Service_Price);
+          total_actual = total_actual + parseInt(json_service[key].Service_Price);
+          total_service = total_service + parseInt(json_service[key].Service_Price);
+        }
+        if(typeof total_actual_all[parseInt(month_service[1])] === 'undefined') {
+          total_actual_all[parseInt(month_service[1])] = parseInt(json_service[key].Service_Price);
+        }
+        else {
+          total_actual_all[parseInt(month_service[1])] = total_actual_all[parseInt(month_service[1])] + parseInt(json_service[key].Service_Price);
         }
       });
-      console.log(actual);
+      var total_actual_permonth = [];
+      for (let i = 0; i < total_actual_all.length; i++) {
+        total_actual_permonth[i] = total_actual_all[i];
+        if(typeof total_actual_all[i] === 'undefined') {
+          total_actual_permonth[i] = 0;
+        }
+        else {
+          total_actual_permonth[i] = total_actual_all[i];
+        }
+      }
+      create_dailyreportChart(total_actual_permonth);
+      create_budgetvsactualChart(total_actual);
+      var label_monthly = ["Bensin", "Sewa Kendaraan", "Uang Parkir", "Uang Isi Angin", "Service", "Lainnya"];
+      var data_monthly = [total_bensin, 0, total_parkir, total_angin, total_service, total_lain];
+      console.log(total_actual_permonth);
+      data_monthly.forEach(function (value, i) {
+        if(parseInt(value) == 0){
+          label_monthly.splice(i, 1);
+          data_monthly.splice(i, 1);
+        }
+      });
+      create_montlycostChart(label_monthly, data_monthly);
     }
     else{
       if((Object.keys(json_travel).length > 0 || kosong == 1) && Object.keys(json_request).length > 0 && Object.keys(json_users).length > 0){
@@ -262,6 +299,31 @@
         $('#loading_onprogress_order').hide();
       }
     }
+    if(nama_var == 'request'){
+      var request_today = 0;
+      var request_month = 0;
+      var request_year = 0;
+      var request_all = 0;
+      var json_request_key = Object.keys(json_request);
+      json_request_key.forEach(function(key) {
+        if(json_request[key].Tanggal_Berangkat == "<?php echo date('d-m-Y'); ?>"){
+          request_today++;
+        }
+        var date_request = json_request[key].Tanggal_Berangkat;
+        var date_request_arr = date_request.split("-");
+        if(date_request_arr[1]+"-"+date_request_arr[2] == "<?php echo date('m-Y'); ?>"){
+          request_month++;
+        }
+        if(date_request_arr[2] == "<?php echo date('Y'); ?>"){
+          request_year++;
+        }
+        request_all++;
+      });
+      $(".total-request-today").html(request_today);
+      $(".total-request-month").html(request_month);
+      $(".total-request-year").html(request_year);
+      $(".total-request-all").html(request_all);
+    }
   }
 
   function open_detail(btn) {
@@ -281,211 +343,241 @@
   }
 	//https://coolors.co/d33f49-21fa90-0c090d-30bced-303036
 	<?php
-		for ($i=7; $i >= 0; $i--) { 
-			$date_tmp = date('Y-m-d', strtotime('-'.$i.' day'));
-			$date[] 	= $date_tmp;
-			$date_tmp_pretty = date('d-m-Y', strtotime('-'.$i.' day'));
-			$date_pretty[] 		= $date_tmp_pretty;
-			// $d 				= new \DateTime($date_tmp);
-			$d 				= new \DateTime($date_tmp);
-			$mst_tmp 	=$d->getTimestamp().substr($d->format('u'),0,3); // millisecond timestamp
-			$mts[] 		= $mst_tmp; // millisecond timestamp
-		}
+    $months = array(
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July ',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    );
+    $this_month = date("n") - 1;
+
+		for ($i=0; $i <= $this_month; $i++) { 
+      $date_pretty[] 		= $months[$i];
+    }
 	?>
-	var dailyreportChart = new Chart(document.getElementById('dailyreportChart').getContext('2d'), {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-      labels: ['<?php echo join("', '", $date_pretty) ?>'],
-      datasets: [{
-        label: 'Actual Cost',
-        borderColor: 'rgba(0, 123, 255, 1)',
-        backgroundColor: 'rgba(0, 123, 255, 0.1)',
-        data: [235000, 435000, 366000, 444000, 765000, 266000, 448000, 569000],
-        lineTension: 0,
-    		fill: true
-      },],
-    },
-
-    // Configuration options go here
-    options: {
-    	title: {
-        display: true,
-        fontSize: 22,
-        text: 'Daily Report'
-      },
-      legend: {
-        display: false
-      },
-      tooltips: {
-        displayColors: false,
-        callbacks: {
-            label: function (tooltipItems, data) {
-                return 'Actual Cost: Rp. ' + tooltipItems.yLabel;
-            }
-        }
-      },
-      scales: {
-        yAxes: [{ 
-          scaleLabel: {
-            display: true,
-            labelString: "Total Actual Cost (Rp.)",
-            fontStyle: "bold"
-          },
-          gridLines: {
-            display: true,
-            drawBorder: true,
-            drawOnChartArea: false,
-          }
-        }],
-        xAxes: [{ 
-          scaleLabel: {
-            display: true,
-            labelString: "Date",
-            fontStyle: "bold"
-          },
-          gridLines: {
-            display: true,
-            drawBorder: true,
-            drawOnChartArea: false,
-          }
-        }]
-      }
+  var dailyreportChart;
+  function create_dailyreportChart(data_data) {
+    if (dailyreportChart != undefined || dailyreportChart !=null) {
+      dailyreportChart.destroy();
     }
-	});
+    dailyreportChart = new Chart(document.getElementById('dailyreportChart').getContext('2d'), {
+      // The type of chart we want to create
+      type: 'line',
 
-  var ctx2 = document.getElementById('montlycostChart').getContext('2d');
-  var grd2 = ctx2.createLinearGradient(500, 0, 0, 0);
-  grd2.addColorStop(0, "rgba(0, 123, 255, 1)");
-  grd2.addColorStop(1, "rgba(0, 123, 255, 0.3)");
-
-	var budgetvsactualChart = new Chart(document.getElementById('budgetvsactualChart').getContext('2d'), {
-		type: 'horizontalBar',
-    data: {
-      labels: ["Budget", "Actual"],
-      datasets: [
-        {
-          label: "Total",
-          backgroundColor: grd2,
-          borderColor: "rgba(0, 123, 255, 1)",
-          borderWidth: 1,
-          data: [526700 , 400000]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        fontSize: 22,
-        text: 'Budget vs Actual'
+      // The data for our dataset
+      data: {
+        labels: ['<?php echo join("', '", $date_pretty) ?>'],
+        datasets: [{
+          label: 'Actual Cost',
+          borderColor: 'rgba(0, 123, 255, 1)',
+          backgroundColor: 'rgba(0, 123, 255, 0.1)',
+          data: data_data,
+          lineTension: 0,
+          fill: true
+        },],
       },
-      tooltips: {
-        displayColors: false,
-        callbacks: {
-            label: function (tooltipItems, data) {
-                return 'Cost: Rp. ' + tooltipItems.xLabel;
-            }
-        }
-      },
-      scales: {
-        xAxes: [{ 
-          display: false,
-          scaleLabel: {
-            display: true,
-            labelString: "Total (Rp.)",
-            fontStyle: "bold"
-          },
-          ticks: {
-            beginAtZero: true
-          },
-          gridLines: {
-            display: true,
-            drawBorder: false,
-            drawOnChartArea: false,
-          },
-        }],
-        yAxes: [{
-          gridLines: {
-            display: true,
-            drawBorder: false,
-            drawOnChartArea: false,
-          },
-          barThickness: 35,
-        }]
-      }
-    }
-	});
 
-  var ctx = document.getElementById('montlycostChart').getContext('2d');
-  var grd = ctx.createLinearGradient(0, 0, 0, 200);
-  grd.addColorStop(0, "rgba(0, 123, 255, 1)");
-  grd.addColorStop(1, "rgba(0, 123, 255, 0.3)");
-
-	var montlycostChart = new Chart(document.getElementById('montlycostChart').getContext('2d'), {
-		type: 'bar',
-    data: {
-      labels: ["Bensin", "Sewa Kendaraan", "Service", "Lainnya"],
-      datasets: [
-        {
-          backgroundColor: grd,
-          borderColor: "rgba(0, 123, 255, 1)",
-          borderWidth: 1,
-          data: [453000,400000,635000,543000]
+      // Configuration options go here
+      options: {
+        title: {
+          display: true,
+          fontSize: 22,
+          text: 'Daily Report'
         },
-      ]
-    },
-    options: {
-    	legend: {
-        display: false
-      },
-      tooltips: {
-        displayColors: false,
-        callbacks: {
-            label: function (tooltipItems, data) {
-                return 'Cost: Rp. ' + tooltipItems.yLabel;
+        legend: {
+          display: false
+        },
+        tooltips: {
+          displayColors: false,
+          callbacks: {
+              label: function (tooltipItems, data) {
+                  return 'Actual Cost: Rp. ' + tooltipItems.yLabel;
+              }
+          }
+        },
+        scales: {
+          yAxes: [{ 
+            scaleLabel: {
+              display: true,
+              labelString: "Total Actual Cost (Rp.)",
+              fontStyle: "bold"
+            },
+            gridLines: {
+              display: true,
+              drawBorder: true,
+              drawOnChartArea: false,
             }
+          }],
+          xAxes: [{ 
+            scaleLabel: {
+              display: true,
+              labelString: "Date",
+              fontStyle: "bold"
+            },
+            gridLines: {
+              display: true,
+              drawBorder: true,
+              drawOnChartArea: false,
+            }
+          }]
         }
-      },
-      title: {
-        display: true,
-        fontSize: 22,
-        text: 'Monthly Cost Actual'
-      },
-      scales: {
-        yAxes: [{ 
-          display: false,
-          scaleLabel: {
-            display: true,
-            labelString: "Total Cost (Rp.)",
-            fontStyle: "bold"
-          },
-          gridLines: {
-            display: true,
-            drawBorder: true,
-            drawOnChartArea: false,
-          },
-          ticks: {
-            beginAtZero: true
-          }
-        }],
-        xAxes: [{ 
-          scaleLabel: {
-            display: true,
-            labelString: "Type",
-            fontStyle: "bold"
-          },
-          barThickness: 35,
-          gridLines: {
-            display: true,
-            drawBorder: false,
-            drawOnChartArea: false,
-          }
-        }]
       }
+    });
+	};
+
+
+  var budgetvsactualChart;
+  function create_budgetvsactualChart(total_actual) {
+    if (budgetvsactualChart != undefined || budgetvsactualChart !=null) {
+      budgetvsactualChart.destroy();
     }
-	});
+
+    var ctx2 = document.getElementById('budgetvsactualChart').getContext('2d');
+    var grd2 = ctx2.createLinearGradient(500, 0, 0, 0);
+    grd2.addColorStop(0, "rgba(0, 123, 255, 1)");
+    grd2.addColorStop(1, "rgba(0, 123, 255, 0.3)");
+    budgetvsactualChart = new Chart(document.getElementById('budgetvsactualChart').getContext('2d'), {
+      type: 'horizontalBar',
+      data: {
+        labels: ["Budget", "Actual"],
+        datasets: [
+          {
+            label: "Total",
+            backgroundColor: grd2,
+            borderColor: "rgba(0, 123, 255, 1)",
+            borderWidth: 1,
+            data: [0 , total_actual]
+          }
+        ]
+      },
+      options: {
+        legend: { display: false },
+        title: {
+          display: true,
+          fontSize: 22,
+          text: 'Budget vs Actual'
+        },
+        tooltips: {
+          displayColors: false,
+          callbacks: {
+              label: function (tooltipItems, data) {
+                  return 'Cost: Rp. ' + tooltipItems.xLabel;
+              }
+          }
+        },
+        scales: {
+          xAxes: [{ 
+            display: false,
+            scaleLabel: {
+              display: true,
+              labelString: "Total (Rp.)",
+              fontStyle: "bold"
+            },
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true,
+              drawBorder: false,
+              drawOnChartArea: false,
+            },
+          }],
+          yAxes: [{
+            gridLines: {
+              display: true,
+              drawBorder: false,
+              drawOnChartArea: false,
+            },
+            barThickness: 35,
+          }]
+        }
+      }
+    });
+  }
+
+  var montlycostChart;
+  function create_montlycostChart(data_label, data_data) {
+    if (montlycostChart != undefined || montlycostChart !=null) {
+      montlycostChart.destroy();
+    }
+    var ctx = document.getElementById('montlycostChart').getContext('2d');
+    var grd = ctx.createLinearGradient(0, 0, 0, 200);
+    grd.addColorStop(0, "rgba(0, 123, 255, 1)");
+    grd.addColorStop(1, "rgba(0, 123, 255, 0.3)");
+
+    montlycostChart = new Chart(document.getElementById('montlycostChart').getContext('2d'), {
+      type: 'bar',
+      data: {
+        labels: data_label,
+        minHeight: 2000,
+        datasets: [
+          {
+            backgroundColor: grd,
+            borderColor: "rgba(0, 123, 255, 1)",
+            borderWidth: 1,
+            data: data_data,
+          },
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        tooltips: {
+          displayColors: false,
+          callbacks: {
+              label: function (tooltipItems, data) {
+                  return 'Cost: Rp. ' + tooltipItems.yLabel;
+              }
+          }
+        },
+        title: {
+          display: true,
+          fontSize: 22,
+          text: 'Monthly Cost Actual'
+        },
+        scales: {
+          yAxes: [{ 
+            display: false,
+            scaleLabel: {
+              display: true,
+              labelString: "Total Cost (Rp.)",
+              fontStyle: "bold"
+            },
+            gridLines: {
+              display: true,
+              drawBorder: true,
+              drawOnChartArea: false,
+            },
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{ 
+            scaleLabel: {
+              display: true,
+              labelString: "Type",
+              fontStyle: "bold"
+            },
+            barThickness: 35,
+            gridLines: {
+              display: true,
+              drawBorder: false,
+              drawOnChartArea: false,
+            }
+          }]
+        }
+      }
+    });
+  }
+  
 
 </script>
